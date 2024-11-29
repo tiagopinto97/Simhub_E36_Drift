74d4c07575f026c0c3b76a20b99e432f2800af30
tiagopinto97 <tiagopt1997@gmail.com>
2024-11-29 08:56:09 +0000

    Code cleanup
    Clean up code, add comments so it makes sense to me when I upgrade my cluster

--- SHCustomProtocol.h
+++ SHCustomProtocol.h
@@ -10,66 +10,35 @@ class SHCustomProtocol
     Tone speedtone;
 
   public:
-    /*
-      CUSTOM PROTOCOL CLASS
-      SEE https://github.com/zegreatclan/SimHub/wiki/Custom-Arduino-hardware-support
+    const int SPEED_PIN = 3; 
+    const int RPM_PIN = 6;
+  
+    static const int RPM_SAMPLE_COUNT = 11;
+    const int RPM_FREQS[RPM_SAMPLE_COUNT] = {31, 31, 45, 79, 110, 141, 178, 206, 224, 255, 283};
+    const int RPM_VALUES[RPM_SAMPLE_COUNT] = {1000, 1500, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
 
-      GENERAL RULES :
-      - ALWAYS BACKUP THIS FILE, reinstalling/updating SimHub would overwrite it with the default version.
-      - Read data AS FAST AS POSSIBLE in the read function
-      - NEVER block the arduino (using delay for instance)
-      - Make sure the data read in "read()" function READS ALL THE DATA from the serial port matching the custom protocol definition
-      - Idle function is called hundreds of times per second, never use it for slow code, arduino performances would fall
-      - If you use library suspending interrupts make sure to use it only in the "read" function when ALL data has been read from the serial port.
-        It is the only interrupt safe place
+    static const int SPEED_SAMPLE_COUNT = 49;
+    const int SPEED_VALUES[SPEED_SAMPLE_COUNT] = {22, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500};
+    const int SPEED_FREQS[SPEED_SAMPLE_COUNT] = {31, 39, 51, 64, 76, 87, 97, 112, 125, 138, 150, 162, 177, 192, 205, 219, 232, 246, 258, 271, 285, 295, 307, 320, 332, 345, 358, 370, 383, 395, 408, 421, 433, 446, 458, 471, 484, 496, 509, 521, 534, 547, 559, 572, 585, 597, 610, 622, 635};
 
-      COMMON FUNCTIONS :
-      - FlowSerialReadStringUntil('\n')
-        Read the incoming data up to the end (\n) won't be included
-      - FlowSerialReadStringUntil(';')
-        Read the incoming data up to the separator (;) separator won't be included
-      - FlowSerialDebugPrintLn(string)
-        Send a debug message to simhub which will display in the log panel and log file (only use it when debugging, it would slow down arduino in run conditions)
-
-    */
-    // int lowbeam;
-    // int highbeams;
-    // int blinkerLeft;
-    // int blinkerRight;
-    // int frontAux; // FOG LIGHT
-    // int battery;
-    // int oilPressure;
-    // int absActive;
-    // int parkingBrake;
-
-    // Set PINS
-    // FROM SIMHUB
-    // PIN 2 - Speed
-    // PIN 4 - RPM0
-    // PIN 5 - TEMP
-    // PIN 6 - Fuel
-
-    int speed_PIN = 3;
-    int rpm_PIN = 6;
-    int tmp_PIN = 5;
-    int fuel_PIN = 2;
 
     
-    void setup()
-    {
-
-      pinMode(speed_PIN, OUTPUT);
-      pinMode(rpm_PIN, OUTPUT);
+    void setup() {
+      pinMode(SPEED_PIN, OUTPUT);
+      pinMode(RPM_PIN, OUTPUT);
 
       rpmtone = Tone();
       speedtone = Tone();
-      rpmtone.begin(rpm_PIN);
-      speedtone.begin(speed_PIN);
+      rpmtone.begin(RPM_PIN);
+      speedtone.begin(SPEED_PIN);
     }
 
-    // Called when new data is coming from computer
-    void read()
-    {
+    /**
+     * Read function, called whenever SimHub sends new data to the arduino
+     * 
+     * After processing the sent values, calls speedProcessing and rpmProcessing to generate the correct frequency for those pins
+     */
+    void read() {
       byte parkingBrake = FlowSerialReadStringUntil(';').toInt();
       double speedKMH = FlowSerialReadStringUntil(';').toDouble();
       double wheel1spd = FlowSerialReadStringUntil(';').toDouble();
@@ -80,42 +49,48 @@ class SHCustomProtocol
       double maxRPMS2 = FlowSerialReadStringUntil(';').toDouble();
       double rpms = FlowSerialReadStringUntil(';').toDouble();
 
-      speedProcessing(speedKMH, wheel1spd, wheel2spd, wheel3spd, wheel4spd);
+      speedProcessing(speedKMH, wheel1spd, wheel2spd, wheel3spd, wheel4spd, parkingBrake);
       rpmProcessing(maxRPMS, rpms);
     }
 
 
-    int generateRPMFrequency(double rpms)
-    {
-      // Define the RPM frequency pairs
-      int freqTable[] = {31, 31, 45, 79, 110, 141, 178, 206, 224, 255, 283};
-      int rpmSteps[] = {1000, 1500, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
-
-      // If RPM is out of range
-      if (rpms <= 1000)  {
+  /**
+   * Function that based on the value will calculate the correct frequency
+   * Receives:
+   * value - to represent ( Either speed or RPM ), 
+   * VALUES and FREQS array - Relation between the value and the frequency that corresponds to
+   * SAMPLE_COUNT - instead of always calculating the sizeof VALUES or FREQS, this field saves that little bit of computing
+   */
+    int generateFrequency(double value, int VALUES[], int FREQS[], int SAMPLE_COUNT) {
+      if (value <= VALUES[0])  {
         return 0;
       }
-      if (rpms >= 10000) {
-        return freqTable[10];
+      if (value >= VALUES[SAMPLE_COUNT - 1]) {
+        return FREQS[SAMPLE_COUNT - 1];
       }
 
-      // Find the correct range
-      for (int i = 0; i < 11; i++)
-      {
-        if (rpms < rpmSteps[i + 1])
-        {
-          // Calculate frequency using integer math
-          double diffRpm = rpms - rpmSteps[i];
-          int diffFreq = freqTable[i + 1] - freqTable[i];
-          int rpmGap = rpmSteps[i + 1] - rpmSteps[i];
-          return freqTable[i] + (diffFreq * diffRpm) / rpmGap;
+      for (int i = 0; i < SAMPLE_COUNT; i++) {
+        if (value < VALUES[i + 1]) {
+          double diffRpm = value - VALUES[i];
+          int diffFreq = FREQS[i + 1] - FREQS[i];
+          int valueGap = VALUES[i + 1] - VALUES[i];
+          return FREQS[i] + (diffFreq * diffRpm) / valueGap;
         }
       }
 
-       return 200; // Fallback, though it should never reach here
+      return 0;
     }
-    void rpmProcessing(double maxRPMS, int rpms)
-    {
+
+  /**
+   * Function that based on the reported maxRPMS and current rpms sets the value on the speedometer
+   * It's limited to 10000 Rpm due to me only beign able to push my speedometer to that value (Stock it's 7000)
+   * This limit is needed mostly because EV's on Forza and other games go well beyond (18000 for ex) and 
+   * it does convert it so the speedometer won't receive a frequency for above 10000
+   * 
+   * maxRPMS - Maximum RPMs reported by SIMHUB
+   * rpms - Current RPMs reported by SIMHUB
+   */
+    void rpmProcessing(double maxRPMS, double rpms) {
       double ratio = 1;
 
       // USE RATIO TO KEEP SPEEDO ALIVE
@@ -124,47 +99,25 @@ class SHCustomProtocol
       }
 
       double speedometerRpms = rpms * ratio;
-      rpmtone.play(generateRPMFrequency(speedometerRpms), uint32_t(200));
+      rpmtone.play(generateFrequency(speedometerRpms, RPM_VALUES, RPM_FREQS, RPM_SAMPLE_COUNT), uint32_t(200));
 
     }
 
 
 
-    int generateSpeedFrequency(int speed)
-    {
-      // Define the RPM frequency pairs
-      int speedSteps[] = {22, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500};
-      int freqTable[] = {31, 39, 51, 64, 76, 87, 97, 112, 125, 138, 150, 162, 177, 192, 205, 219, 232, 246, 258, 271, 285, 295, 307, 320, 332, 345, 358, 370, 383, 395, 408, 421, 433, 446, 458, 471, 484, 496, 509, 521, 534, 547, 559, 572, 585, 597, 610, 622, 635};
-
-
-      // If RPM is out of range
-      if (speed <= 22)
-      {
-        return 0;
-      }
-      if (speed >= 500)
-      {
-        return 635;
-      }
-
-      // Find the correct range
-      for (int i = 0; i < 51; i++)
-      {
-        if (speed < speedSteps[i + 1])
-        {
-          // Calculate frequency using integer math
-          int diffSpd = speed - speedSteps[i];
-          int diffFreq = freqTable[i + 1] - freqTable[i];
-          int spdGap = speedSteps[i + 1] - speedSteps[i];
-          return freqTable[i] + (diffFreq * diffSpd) / spdGap;
-        }
-      }
-
-      // return 0; // Fallback, though it should never reach here
-    }
-
-    void speedProcessing(double speedKMH, double wheel1spd, double wheel2spd, double wheel3spd, double wheel4spd)
-    {
+  /**
+   * Function responsible for processing the speed signal.
+   * It will show the value for the fastest spinning wheel, wich is the main reason behind this specific protocol.
+   * That is the most realistic behaviour for when a car is doing a burnout or drifting, where usualy the game reports the 
+   * vehicle speed (0 for burnout, less than wheelspeed for drifting) but I want always wheelspeed.
+   * 
+   * TODO - On RWD cars when parking brake is on return as speed is zero (for drifting purposes)
+   * 
+   * speedKMH - Fallback in case the game does not report per wheel speed
+   * wheel1spd | wheel2spd | wheel3spd | wheel4spd - specific wheel speed, used calculate the speed value
+   * parkingBrake - byte to indicate if parking brake is on
+   */
+    void speedProcessing(double speedKMH, double wheel1spd, double wheel2spd, double wheel3spd, double wheel4spd, byte parkingBrake) {
       double maxSpd = speedKMH;
       if (maxSpd < wheel1spd) {
         maxSpd = wheel1spd;
@@ -178,29 +131,18 @@ class SHCustomProtocol
       if (maxSpd < wheel4spd) {
         maxSpd = wheel4spd;
       }
-      speedtone.play(generateSpeedFrequency(maxSpd), uint32_t(200));
+      speedtone.play(generateFrequency(maxSpd, SPEED_VALUES, SPEED_FREQS, SPEED_SAMPLE_COUNT), uint32_t(200));
     }
 
+    /**
+     * Function needed by SimHub, can be empty
+     */
+    void loop() {}
 
-    void dwPin(int PIN, bool ON_OFF) {
-      digitalWrite(PIN, ON_OFF ? LOW : HIGH);
-    }
-
-
-    void loop()
-    {
-    }
-
-
-
-    // Called once between each byte read on arduino,
-    // THIS IS A CRITICAL PATH :
-    // AVOID ANY TIME CONSUMING ROUTINES !!!
-    // PREFER READ OR LOOP METHOS AS MUCH AS POSSIBLE
-    // AVOID ANY INTERRUPTS DISABLE (serial data would be lost!!!)
-    void idle()
-    {
-    }
+    /**
+     * Function needed by SimHub, can be empty
+     */
+    void idle() {}
 };
 
 #endif
