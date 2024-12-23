23ec851caba9f4e3c435f81df143e19c400331bc
Tiago Pinto <tiagopt1997@gmail.com>
2024-12-23 11:47:05 +0000

    Update SHCustomProtocolJS.js
    Many improvements over last version
    Game Detection( to know the custom variables for the calculations)
    RWD|FWD Detection (to improve performance)
    Configurable MAX_RPM
    Configurable SPEED_SPLIT
    Car id detection ( to recalculate values)
    Added RPM Multiplication for games like EuroTruckSimulator ( under 3000 max rpm)
    Speed ratio calculation ( as angular speed it's not trully matched with speed (at least not on all of the games I tested) 
    
    Added Forza, Assetto and EA SPORTS code directly

--- SHCustomProtocolJS.js
+++ SHCustomProtocolJS.js
@@ -1,37 +1,106 @@
-// Variables to send to arduino
-var reportSpeed = 0;
-var reportRpm = 0;
+// RUN ONCE JAVASCRIPT CODE
+var IS_CAR_RWD = null;
+var RPM_RATIO = 1;
+var MAX_RPM = 10000;
+var CAR_ID = '';
+var GAME_CODE_ID = -1;
+var SPEED_RATIO = 1;
+const SPEED_SPLIT = 300;
 
-// Speed Processing
-const handbrakeOn = $prop('JoystickPlugin.handbrake_Slider0') > 0;
-const speed = $prop('SpeedKmh');
+const GAME_CODE_PROPS = [
+  { //Forza Horizon 5
+    fl: 'GameRawData.WheelRotationSpeedFrontLeft',
+    fr: 'GameRawData.WheelRotationSpeedFrontRight',
+    rl: 'GameRawData.WheelRotationSpeedRearLeft',
+    rr: 'GameRawData.WheelRotationSpeedRearRight'
+  },
+  { // Assetto Corsa
+    fl: 'GameRawData.Physics.WheelAngularSpeed01',
+    fr: 'GameRawData.Physics.WheelAngularSpeed02',
+    rl: 'GameRawData.Physics.WheelAngularSpeed03',
+    rr: 'GameRawData.Physics.WheelAngularSpeed04'
+  },
+  { // EA SPORTS WRC
+    fl: 'GameRawData.SessionUpdate.vehicle_cp_forward_speed_fl',
+    fr: 'GameRawData.SessionUpdate.vehicle_cp_forward_speed_fr',
+    rl: 'GameRawData.SessionUpdate.vehicle_cp_forward_speed_bl',
+    rr: 'GameRawData.SessionUpdate.vehicle_cp_forward_speed_br',
+    fl: 'SpeedKmh', // Override FL, because forward speed is bad here
+  }
+];
 
-// ForzaHorizon5, Assetto Corsa, EA SPORTS WRC
-const flspeed = Math.max($prop('GameRawData.WheelRotationSpeedFrontLeft'), $prop('GameRawData.Physics.WheelAngularSpeed01'), $prop('GameRawData.SessionUpdate.vehicle_cp_forward_speed_fl'), 0);
-const frspeed = Math.max($prop('GameRawData.WheelRotationSpeedFrontRight'), $prop('GameRawData.Physics.WheelAngularSpeed02'), $prop('GameRawData.SessionUpdate.vehicle_cp_forward_speed_fr'), 0);
-const rlspeed = Math.max($prop('GameRawData.WheelRotationSpeedRearLeft'), $prop('GameRawData.Physics.WheelAngularSpeed03'), $prop('GameRawData.SessionUpdate.vehicle_cp_forward_speed_bl'), 0);
-const rrspeed = Math.max($prop('GameRawData.WheelRotationSpeedRearRight'), $prop('GameRawData.Physics.WheelAngularSpeed04'), $prop('GameRawData.SessionUpdate.vehicle_cp_forward_speed_br'), 0);
 
-// Validate if car is RWD only if it's moving and handbrake is not on
-if (speed > 5 && !handbrakeOn) {
-  IS_CAR_RWD = Math.max(rlspeed, rrspeed) > Math.max(flspeed, frspeed);
-}
-
-// Generate the report speed (If RWD and handbrake on, use only rear wheels, else)
-if (IS_CAR_RWD && handbrakeOn) {
-  reportSpeed = Math.max(rlspeed, rrspeed);
-} else {
-  const wheelSpeed = Math.max(flspeed, frspeed, rlspeed, rrspeed);
-  reportSpeed = (wheelSpeed > 0) ? wheelSpeed : speed; // speed is a falback for unsupported games  
-}
-
+// JAVASCRIPT
+const SPEED = $prop('SpeedKmh');
+const RPMS = $prop('Rpms');
+const CURRENT_CAR_ID = $prop('CarId');
+const BRAKE = $prop('Brake');
+let reportRpm = 0;
 
 // RPM Processing
-const maxrpms = $prop('MaxRpm');
-const rpms = $prop('Rpms');
+reportRpm = RPMS * RPM_RATIO;
 
-const ratio = (maxrpms > 10000) ? 10000 / maxrpms : 1;
+// Reset every "Constant" if CarId changes
+if (CAR_ID != CURRENT_CAR_ID) {
+  CAR_ID = CURRENT_CAR_ID;
+  IS_CAR_RWD = null;
+  RPM_RATIO = 1;
+  GAME_CODE_ID = -1;
+  MAX_RPM = 10000;
+  SPEED_RATIO = 1;
+}
 
-reportRpm = rpms * ratio;
 
-return `${reportSpeed};${reportRpm};`; 
+if (GAME_CODE_ID === -1) {
+  // Detect wich game is playing
+  let gameIndex = 0;
+  while (GAME_CODE_ID === -1 && gameIndex < Object.keys(GAME_CODE_PROPS).length) {
+    if ($prop(GAME_CODE_PROPS[gameIndex].fl) != null) {
+      GAME_CODE_ID = gameIndex;
+    }
+    gameIndex++;
+  }
+  // On this check just send the speedKmh to avoid multiple if's
+  return `${SPEED};${reportRpm};`;
+} else {
+  // Calculate based on selected game
+  
+  let frontSpeed = 0;
+  let rearSpeed = 0;
+  let reportSpeed = 0;
+  if (!IS_CAR_RWD || IS_CAR_RWD === null) {
+    frontSpeed = Math.max(
+      Math.abs($prop(GAME_CODE_PROPS[GAME_CODE_ID].fl)),
+      Math.abs($prop(GAME_CODE_PROPS[GAME_CODE_ID].fr)),
+    );
+  }
+  if (IS_CAR_RWD || IS_CAR_RWD === null) {
+    rearSpeed = Math.max(
+      Math.abs($prop(GAME_CODE_PROPS[GAME_CODE_ID].rl)),
+      Math.abs($prop(GAME_CODE_PROPS[GAME_CODE_ID].rr)),
+    );
+  }
+
+  // Validate if car is RWD only if it's moving and handbrake is not on
+  if (IS_CAR_RWD == null && SPEED > 2 && !($prop('JoystickPlugin.handbrake_Slider0') > 0) &&  BRAKE < 0.1) {
+    MAX_RPM = $prop('MaxRpm');
+    RPM_RATIO = (MAX_RPM > 10000) ? 10000 / MAX_RPM : (MAX_RPM < 3000 /*Euro Truck Simulator 2 */) ? 7500 / MAX_RPM  : 1;
+    IS_CAR_RWD = rearSpeed > frontSpeed;
+  }
+
+	// Generate the report speed based on driven wheels
+	const angularSpeed = ((IS_CAR_RWD) ? rearSpeed : frontSpeed);
+	reportSpeed = angularSpeed * SPEED_RATIO;
+	const diff = SPEED - reportSpeed;
+	
+	// Calculate speed ratio (IF SPEED is above rearSpeed)
+	if ( BRAKE < 0.1 && SPEED > 50 && angularSpeed > 30 && SPEED_RATIO < 1.2 && diff > 2 && diff < 5 ) {
+		const NEW_SPD_RATIO = SPEED / angularSpeed;
+		if (NEW_SPD_RATIO < 1.3 && NEW_SPD_RATIO - SPEED_RATIO < 0.05) {
+			SPEED_RATIO = NEW_SPD_RATIO;
+			reportSpeed =  angularSpeed * SPEED_RATIO
+		}
+	}  
+
+  return `${(reportSpeed % SPEED_SPLIT).toFixed(0)};${reportRpm};`;
+}
